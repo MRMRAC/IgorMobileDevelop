3f124ecf3a2e91a4dad8432fff108d17e8088bb4
MRMRAC <124257737+MRMRAC@users.noreply.github.com>
2025-09-25 16:55:59 +0300

    Update orderFunctions.js

--- Второе домашнее задание/orderFunctions.js
+++ Второе домашнее задание/orderFunctions.js
@@ -1,28 +1,36 @@
+
 import { orders } from "./data.js";
 
-// Заказы пользователя
+// получение заказов пользователя
 export function getUserOrders(userId) {
-  return orders.filter(o => o.userId === userId);
+  return orders.filter(order => order.userId === userId);
 }
 
-// Добавить товар в заказ
+// Добавление товара в заказ
 export function addProductToOrder(orderId, newProduct) {
+  
   const order = orders.find(o => o.id === orderId);
   if (!order) return null;
+
   order.products = [...order.products, newProduct];
   return order;
 }
 
-// Сводка по заказу
+// Получение сводки по заказу
 export function getOrderSummary(orderId) {
+
+  
   const order = orders.find(o => o.id === orderId);
-  if (!order) return null;
+  
+  if (!order){ return null};
 
   const { products, total, status, userId } = order;
+
   return {
     productsCount: products.length,
     total: `$${total.toFixed(2)}`,
     status: status.toUpperCase(),
     userId
   };
+  
 }
